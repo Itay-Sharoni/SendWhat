344758b39e7f36c1dea7ed28d3bb25c929ab4849
Itay Sharoni <110981018+Itay-Sharoni@users.noreply.github.com>
2023-09-04 00:19:20 +0300

    Update events.js

--- src/js/events.js
+++ src/js/events.js
@@ -1,8 +1,8 @@
-document.querySelector('#button').addEventListener('click', redirectWhatsapp);
+document.querySelector('#button').addEventListener('click', redirectSendWhat);
 document.querySelector('#phone').addEventListener('keypress', (event) => {
     if (event.keyCode === 13) {
         event.preventDefault();
-        redirectWhatsapp();
+        redirectSendWhat();
     };
 });
 
